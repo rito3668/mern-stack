c84be9bfb81791e42a9dd8f1978bf6e6f9b8344a
Ritabrata Pal <rito3668@gmail.com>
2023-12-21 09:07:26 +0530

    useSignup hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@
 import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 function App() {
   return (
     <div className="App">
@@ -11,10 +13,15 @@ function App() {
       <div className="pages">
         
         <Switch>
-          <Route path='/'>
+          <Route exact path='/'>
             <Home/>
           </Route>
-
+          <Route path='/login'>
+            <Login/>
+          </Route>
+          <Route path='/signup'>
+            <Signup/>
+          </Route>
         </Switch>
       </div>
     
